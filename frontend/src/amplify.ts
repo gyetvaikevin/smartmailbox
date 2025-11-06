422d8a83a3583e47e76461a7331a6e99d582d328
gyetvaikevin <gyetvaikevin@gmail.com>
2025-11-06 09:39:10 +0100

    cs

--- frontend/src/amplify.ts
+++ frontend/src/amplify.ts
@@ -6,12 +6,15 @@ Amplify.configure({
     region: import.meta.env.VITE_REGION,
     userPoolId: import.meta.env.VITE_USER_POOL_ID,
     userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
-    oauth: {
-      domain: import.meta.env.VITE_COGNITO_DOMAIN,
-      scopes: ["openid", "email", "profile"],
-      redirectSignIn: "http://localhost:5173/auth/callback,https://main.d2y8t9d4z5sf43.amplifyapp.com/auth/callback",
-      redirectSignOut: "http://localhost:5173/,https://main.d2y8t9d4z5sf43.amplifyapp.com/",
-      responseType: "code",
+    loginWith: {
+      email: true,
+      oauth: {
+        domain: import.meta.env.VITE_COGNITO_DOMAIN,
+        scopes: ["openid", "email", "profile"],
+        redirectSignIn: "http://localhost:5173/auth/callback,https://main.d2y8t9d4z5sf43.amplifyapp.com/auth/callback",
+        redirectSignOut: "http://localhost:5173/,https://main.d2y8t9d4z5sf43.amplifyapp.com/",
+        responseType: "code",
+      },
     },
   },
 });
