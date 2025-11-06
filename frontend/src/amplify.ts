47bf412f78f39f8d47ee0e104ce99d6a3f1e70d2
gyetvaikevin <gyetvaikevin@gmail.com>
2025-11-06 10:16:38 +0100

    sdafasd

--- frontend/src/amplify.ts
+++ frontend/src/amplify.ts
@@ -1,8 +1,8 @@
-import { Amplify } from "@aws-amplify/core";
-import "@aws-amplify/auth";
-
 Amplify.configure({
   Auth: {
+    region: import.meta.env.VITE_REGION,
+    userPoolId: import.meta.env.VITE_USER_POOL_ID,
+    userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
     Cognito: {
       region: import.meta.env.VITE_REGION,
       userPoolId: import.meta.env.VITE_USER_POOL_ID,
@@ -12,13 +12,20 @@ Amplify.configure({
         oauth: {
           domain: import.meta.env.VITE_COGNITO_DOMAIN,
           scopes: ["openid", "email", "profile"],
-          redirectSignIn: "http://localhost:5173/auth/callback,https://main.d2y8t9d4z5sf43.amplifyapp.com/auth/callback",
-          redirectSignOut: "http://localhost:5173/,https://main.d2y8t9d4z5sf43.amplifyapp.com/",
+          redirectSignIn: [
+            "http://localhost:5173/auth/callback",
+            "https://main.d2y8t9d4z5sf43.amplifyapp.com/auth/callback"
+          ],
+          redirectSignOut: [
+            "http://localhost:5173/",
+            "https://main.d2y8t9d4z5sf43.amplifyapp.com/"
+          ],
           responseType: "code",
         },
       },
     },
   },
 });
+
 
 console.log("Amplify config:", Amplify.getConfig());
